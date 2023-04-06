779b955aa0434e8735e1494431fe2e2c458c9c2e
Ajay Kumar <Ajay@gmail.com>
2023-04-06 19:17:07 +0530

    EmailJS code added on script.js file

--- js/script.js
+++ js/script.js
@@ -57,3 +57,27 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+/* =========== EmailJS Automation code ============= */
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        subject: document.getElementById("subject").value,
+        email: document.getElementById("email").value,
+        ph_number: document.getElementById("ph_number").value,
+        message: document.getElementById("message").value
+    };
+
+    const serviceID = "service_sk7xsn2";
+    const templateID = "template_fh78ycd"; 
+
+    emailjs.send(serviceID, templateID, params).then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("ph_number").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully");
+    }).catch((err) => console.log(err));
+}
